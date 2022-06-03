feef4bba9f8d2e6a2d2fd94044dc0b95f92d3856
ujahvee <ujahvictor666@gmail.com>
2022-06-03 20:41:29 +0100

    commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+
+    if length == 0:
+        return (None)
+
+    max_int = my_list[0]
+
+    for i in range(1, length):
+        if my_list[i] > top_int:
+            top_int = my_list[i]
+
+    return (top_int)
