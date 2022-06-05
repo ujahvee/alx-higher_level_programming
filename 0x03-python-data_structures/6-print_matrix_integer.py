cb56dce4d825135006d6d19810dededc843173d4
ujahvee <ujahvictor666@gmail.com>
2022-06-05 14:20:01 +0100

    commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for p in range(len(matrix[i])):
-            if j != 0:
+            if p != 0:
                 print(" ", end='')
             print("{:d}".format(matrix[i][p]), end='')
         print()
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if length == 0:
         return (None)
 
-    max_int = my_list[0]
+    top_int = my_list[0]
 
     for i in range(1, length):
         if my_list[i] > top_int:
