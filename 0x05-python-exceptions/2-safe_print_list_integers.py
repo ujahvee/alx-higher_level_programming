326737bdae1604e9feef7081e802a1a1a0b2a3cb
ujahvee <ujahvictor666@gmail.com>
2022-06-20 19:59:26 +0100

    commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    sumed = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except (ValueError, TypeError):
+            continue
+        sumed += 1
+    print()
+    return (sumed)
