2eed39f6900e5a29ac86ef66d49b367dea2178b2
ujahvee <ujahvictor666@gmail.com>
2022-06-20 19:36:10 +0100

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
         try:
-            print(f'{my_list[idx]}', end='')
+            print(f'{my_list[i]}', end='')
 
         except (IndexError):
             continue
