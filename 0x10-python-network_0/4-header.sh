76ad42d16b991d4a7a2391df7510505f3d251253
ujahvee <ujahvictor666@gmail.com>
2022-09-23 04:38:07 +0100

    commit

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request to the URL, and displays the body of the response.
-curl -sX GET -H "X-School-User-Id: 98" "$1"
+# A script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl "$1" -s -X GET -H "X-HolbertonSchool-User-Id: 98"
